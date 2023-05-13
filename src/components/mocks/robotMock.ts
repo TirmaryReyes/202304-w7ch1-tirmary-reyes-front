b3b414fe98d254111333ec18863606cc17c65fa5
Tirmary Reyes <tmrv1991@gmail.com>
2023-05-13 15:33:05 +0200

    Add robotMock to test loadRobot

---
+++ src/components/mocks/robotMock.ts
@@ -0,0 +1,15 @@
+const robotMock = [
+  {
+    id: 656,
+    name: "C3PO",
+    imageUrl: "https://example.com/robot1.jpg",
+
+    characteristics: {
+      speed: 8,
+      endurance: 6,
+      createDate: "2023-05-01",
+    },
+  },
+];
+
+export default robotMock;
