b7927a5a1cbdfc0c208edafba74cc33b262f40f9
Carles Pueyo <cpcastells@gmail.com>
2023-05-14 14:01:15 +0200

    Create styles for RobotList component

--- src/components/RobotList/RobotList.tsx
+++ src/components/RobotList/RobotList.tsx
@@ -1,17 +1,20 @@
 import { useAppSelector } from "../../store";
 import { RobotStructure } from "../../types";
 import RobotCard from "../RobotCard/RobotCard";
+import RobotListStyled from "./RobotListStyled";
 
 const RobotList = (): JSX.Element => {
   const robots = useAppSelector((state): RobotStructure[] => state.robotStore);
 
   return (
-    <article className="robotList">
-      <h2 className="list-title">Robots List</h2>
-      {robots.map((robot) => (
-        <RobotCard key={robot.id} robot={robot} />
-      ))}
-    </article>
+    <RobotListStyled>
+      <article className="robotList">
+        <h2 className="list-title">Robots List</h2>
+        {robots.map((robot) => (
+          <RobotCard key={robot.id} robot={robot} />
+        ))}
+      </article>
+    </RobotListStyled>
   );
 };
 
