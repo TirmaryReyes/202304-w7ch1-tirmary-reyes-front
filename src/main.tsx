910282578b0d62c964cf3bd38090732059abb825
Tirmary Reyes <tmrv1991@gmail.com>
2023-05-17 10:23:36 +0200

    Modify main and gitignore

--- src/main.tsx
+++ src/main.tsx
@@ -5,17 +5,12 @@ import App from "./components/App/App";
 import { Provider } from "react-redux";
 import { store } from "./store";
 import GlobalStyle from "./styles/GlobalStyle";
-import RobotCard from "./components/RobotCard/RobotCard";
-import robotMock from "./mocks/robotMock";
-import RobotList from "./components/RobotList/RobotList";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <GlobalStyle />
     <Provider store={store}>
       <App />
-      <RobotList />
-      <RobotCard robot={robotMock[0]} />
     </Provider>
   </React.StrictMode>
 );
